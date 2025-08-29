0d636b441fa5541bea2a3f933a8c7cc23460ae8e
Alex <221934805+Alex5724@users.noreply.github.com>
2025-08-29 10:15:17 +0300

    The script stage 2 for the game.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const container = document.querySelector("#container");
 const size = document.querySelector("#size");
+const clear = document.querySelector("#clear");
 let initialSize = 16;
+
+function standardSize(){
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+
+  const cellWidth = containerWidth / initialSize;
+  const cellHeight = containerHeight / initialSize;
+
+  for (let i = 0; i < Math.pow(initialSize, 2); i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("element");
+    cell.style.width = `${cellWidth}px`;
+    cell.style.height = `${cellHeight}px`;
+    clear.addEventListener('click', () => {
+    cell.style.backgroundColor = '';
+ });
+    cell.addEventListener('click', (event) => {
+    if(event.target.classList.contains("element")){
+      if(!event.target.style.backgroundColor){
+        event.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
+      }
+      else{
+        let currentOpacity = parseFloat(event.target.style.opacity) || 1;
+        if(currentOpacity > 0.1){
+        event.target.style.opacity = currentOpacity - 0.1;
+        }
+      }
+    }
+  });
+    container.appendChild(cell);
+  }
+}
+
+standardSize();
 
 size.addEventListener("click", () => {
     while(true){
@@ -15,6 +50,7 @@ size.addEventListener("click", () => {
     }
 }  
 });
+
 function setSize(n) {
   container.innerHTML = "";
 
@@ -24,11 +60,27 @@ function setSize(n) {
   const cellWidth = containerWidth / n;
   const cellHeight = containerHeight / n;
 
-  for (let i = 0; i < Math.pow(n, 2); i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("element");
-    cell.style.width = `${cellWidth}px`;
-    cell.style.height = `${cellHeight}px`;
-    container.appendChild(cell);
-  }
+for (let i = 0; i < Math.pow(n, 2); i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("element");
+  cell.style.width = `${cellWidth}px`;
+  cell.style.height = `${cellHeight}px`;
+  clear.addEventListener('click', () => {
+  cell.style.backgroundColor = '';
+ });
+  cell.addEventListener('click', (event) => {
+    if(event.target.classList.contains("element")){
+      if(!event.target.style.backgroundColor){
+        event.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
+      }
+      else{
+        let currentOpacity = parseFloat(event.target.style.opacity) || 1;
+        if(currentOpacity > 0.1){
+        event.target.style.opacity = currentOpacity - 0.1;
+        }
+      }
+    }
+  });
+  container.appendChild(cell);
 }
+};
